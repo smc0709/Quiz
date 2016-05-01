99a3853b558f52b90c5b49ba25ecbaab2eb00c79
smc0709 <smc0709@hotmail.com>
2016-05-01 16:26:12 +0200

    Tema 9: Despliegue BBDD en Heroku

--- models/index.js
+++ models/index.js
@@ -6,6 +6,8 @@ var Sequelize = require('sequelize');
 // Usar BBDD SQLite:
 //    DATABASE_URL = sqlite:///
 //    DATABASE_STORAGE = quiz.sqlite
+// Usar BBDD Postgres:
+//    DATABASE_URL = postgres://user:passwd@host:port/database
 
 var url, storage;
 
